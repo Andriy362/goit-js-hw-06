4f28c5243a6df7978fac1983da2e5d5392905ed0
Andriy Pustovit <andriy.pustovit@mail.ru>
2022-07-21 20:39:02 +0300

    ввв

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const newRefs = document.querySelector("#validation-input");
 newRefs.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length == newRefs.dataset.length) {
+  if (event.currentTarget.value.length === Number(newRefs.dataset.length)) {
     newRefs.classList.add("valid");
     newRefs.classList.remove("invalid");
   } else {
